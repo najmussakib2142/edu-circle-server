950959252b589b69b3bb54b851e215248d479fcc
najmussakib2142 <najmussakib2142@gmail.com>
2025-07-16 06:30:22 +0600

    use findone to show detail

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 // middleWare
@@ -35,6 +35,16 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result)
         })
+
+        // specific assignment
+        app.get('/assignments/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await assignmentsCollection.findOne(query)
+            res.send(result)
+        })
+
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
